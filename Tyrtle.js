60aec8fae8b068bae43d9a34cdbedd1378a24a32
Nick Fisher <nick.fisher@numberfour.eu>
2011-09-03 15:59:55 +1000

    First iteration of Tyrtle. Still very basic for now.

---
+++ Tyrtle.js
@@ -0,0 +1,301 @@
+/*!
+ * Myrtle - A JavaScript Unit Testing Framework
+ * 
+ * Copyright (c) 2011 Nick Fisher
+ * Licensed under the Creative Commons BY-SA License
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ */
+/*globals module */
+(function (root) {
+    var Tyrtle, Module, Test, assert,
+        AssertionError, SkipMe,
+        PASS = 0,
+        FAIL = 1,
+        SKIP = 2,
+        extend,
+        defer,
+        noop,
+        each
+    ;
+    extend = function (Cls, obj) {
+        var i;
+        for (i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                Cls.prototype[i] = obj[i];
+            }
+        }
+    };
+    defer = (root && root.postMessage 
+        ? (function () {
+            // credit to David Baron: http://dbaron.org/log/20100309-faster-timeouts
+            var timeouts = [], messageName = "zero-timeout-message", setZeroTimeout, handleMessage;
+
+            setZeroTimeout = function (fn) {
+                timeouts.push(fn);
+                root.postMessage(messageName, "*");
+            };
+
+            handleMessage = function (event) {
+                if (event.source === root && event.data === messageName) {
+                    event.stopPropagation();
+                    if (timeouts.length > 0) {
+                        var fn = timeouts.shift();
+                        fn();
+                    }
+                }
+            };
+            
+            root.addEventListener("message", handleMessage, true);
+            
+            return function (func) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                setZeroTimeout(args.length === 0 ? func : function () {
+                    func.apply(func, args);
+                });
+            };
+        }())
+        : function (func) {
+            return function (func) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                setTimeout(args.length === 0 ? func : function () {
+                    func.apply(func, args);
+                }, 0);
+            };
+        }
+    );
+    noop = function () {};
+    each = function (obj, iterator, context) {
+        if (obj === null || typeof obj === 'undefined') {
+            return;
+        }
+        if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
+            obj.forEach(iterator, context);
+        } else {
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    iterator.call(context, obj[key], key, obj);
+                }
+            }
+        }
+    };
+    //
+    // Tyrtle
+    //
+    (function () {
+        Tyrtle = function (options) {
+            options = options || {};
+            this.modules = [];
+            this.callback = options.callback || noop;
+        };
+        Tyrtle.PASS = PASS;
+        Tyrtle.FAIL = FAIL;
+        Tyrtle.SKIP = SKIP;
+        extend(Tyrtle, {
+            passes : 0,
+            fails : 0,
+            errors : 0,
+            skips : 0,
+            ////
+            module : function (name, body) {
+                this.modules.push(new Module(name, body));
+            },
+            run : function (options) {
+                var runNext,
+                    i = -1,
+                    l = this.modules.length,
+                    tyrtle = this
+                ;
+                
+                runNext = function () {
+                    var mod;
+                    ++i;
+                    if (i === l) {
+                        // all done
+                        tyrtle.callback();
+                    } else {
+                        mod = tyrtle.modules[i];
+                        tyrtle.runModule(mod, function () {
+                            each(['passes', 'fails', 'errors', 'skips'], function (key) {
+                                tyrtle[key] += mod[key];
+                            });
+                            defer(runNext);
+                        });
+                    }
+                };
+                runNext();
+            },
+            runModule : function (mod, callback) {
+                mod.run(callback);
+            }
+        });
+    }());
+    //
+    // Module
+    //
+    (function () {
+        Module = function (name, body) {
+            this.name = name;
+            this.body = body;
+        };
+        extend(Module, {
+            tests : null,
+            passes : 0,
+            fails : 0,
+            skips : 0,
+            errors : 0,
+            test : function (name, fn) {
+                this.tests.push(new Test(name, fn));
+            },
+            run : function (callback) {
+                if (this.tests === null) {
+                    this.tests = [];
+                    this.body();
+                }
+                var runNext,
+                    i = -1,
+                    l = this.tests.length,
+                    mod = this
+                ;
+                runNext = function () {
+                    var test;
+                    ++i;
+                    if (i === l) {
+                        // do the afterall
+                        defer(callback);
+                    } else {
+                        test = mod.tests[i];
+                        mod.runTest(test, function () {
+                            switch (test.status) {
+                            case PASS :
+                                ++mod.passes;
+                                break;
+                            case FAIL :
+                                ++mod.fails;
+                                if (test.error) {
+                                    ++mod.errors;
+                                }
+                                break;
+                            case SKIP :
+                                ++mod.skips;
+                                break;
+                            }
+                            defer(runNext);
+                        });
+                    }
+                };
+                runNext();
+            },
+            runTest : function (test, callback) {
+                // todo: befores and afters here
+                test.run(callback);
+            }
+        });
+    }());
+    
+    (function () {
+        //
+        // Test
+        //
+        Test = function (name, body) {
+            this.name = name;
+            this.body = body;
+        };
+        extend(Test, {
+            status : null,
+            statusMessage: '',
+            runTime : -1,
+            error : null,
+            isAsync : false,
+            run : function (callback) {
+                var start;
+                try {
+                    start = new Date();
+                    this.body.call({}, assert);
+                    this.runTime = new Date() - start;
+                    this.status = PASS;
+                    this.statusMessage = 'Passed';
+                } catch (e) {
+                    this.status = FAIL;
+                    this.statusMessage = "Failed: " + ((e && e.message) || String(e));
+                    if (e instanceof SkipMe) {
+                        this.status = SKIP;
+                    } else if (!(e instanceof AssertionError)) {
+                        this.error = e;
+                    }
+                } finally {
+                    callback(this);
+                }
+            }
+        });
+    }());
+    
+    AssertionError = function (msg) {
+        this.name = "AssertionError";
+        this.message = msg;
+    };
+    
+    SkipMe = function (reason) {
+        this.message = reason;
+    };
+    (function () {
+        var AssertThat, fail, assertions;
+        
+        assert = function (actual) {
+            return new AssertThat(actual);
+        };
+        assert.that = assert;
+        
+        fail = function (message, args, userMessage) {
+            throw new AssertionError(message);
+        };
+        
+        AssertThat = function (actual) {   
+            this.actual = actual;
+        };
+        assertions = {
+            is : function (expected) {
+                var actual, f;
+                actual = this.actual;
+                f = function (message) {
+                    if (actual !== expected) {
+                        fail(
+                            "Actual value {0} did not match expected value {1}",
+                            [actual, expected],
+                            message
+                        );
+                    }
+                };
+                f.since = f;
+                return f;
+            },
+            not : function (unexpected) {
+                var actual, f;
+                actual = this.actual;
+                f = function (message) {
+                    if (actual === unexpected) {
+                        fail(
+                            "Actual value matched the unexpected value {0}",
+                            [actual],
+                            message
+                        );
+                    }
+                };
+                f.since = f;
+                return f;
+            }
+        };
+        each(assertions, function (fn, key) {
+            if (key !== 'is') {
+                assertions.is[key] = fn;
+            }
+        });
+        extend(AssertThat, assertions);
+    }());
+//#JSCOVERAGE_IF
+    if (typeof module !== 'undefined') {
+        module.exports = Tyrtle;
+    } else {
+        root.Tyrtle = Tyrtle;
+    }
+    
+}(this));
---
+++ test/tyrtle-tests.js
@@ -0,0 +1,111 @@
+jQuery(function ($) {
+    asyncTest("Basic empty tests are reported as success", function () {
+        expect(4);
+        
+        var t = new Tyrtle({
+            callback : function () {
+                equal(this.passes, 1, "The test should have been marked as a pass.");
+                equal(this.fails, 0, "No test should have failed");
+                equal(this.errors, 0, "No errors");
+                equal(this.skips, 0, "None skipped");
+                start();
+            }
+        });
+        t.module("tests", function () {
+            this.test("empty", function () {});
+        });
+        t.run();
+    });
+    asyncTest("Errors are reported as failures", function () {
+        expect(4);
+        
+        var t = new Tyrtle({
+            callback : function () {
+                equal(this.passes, 0, "Should not have passed");
+                equal(this.fails, 1, "It should have failed");
+                equal(this.errors, 1, "It should be marked as an error");
+                equal(this.skips, 0, "None skipped");
+                start();
+            }
+        });
+        t.module("tests", function () {
+            this.test("erroring", function () {
+                var x = 0;
+                x();
+            });
+        });
+        t.run();
+    });
+    asyncTest("Basic assertions", function () {
+        var t = new Tyrtle({
+            callback : function () {
+                var i, l, results, expected, test, undef;
+                results = [
+                    'PASS',
+                    'PASS',
+                    'PASS',
+                    'FAIL',
+                    'PASS',
+                    'FAIL'
+                ];
+                for (i = 0, l = results.length; i < l; ++i) {
+                    test = t.modules[0].tests[i];
+                    expected = results[i];
+                    equal(
+                        test.status,
+                        Tyrtle[expected],
+                        "Test " + test.name + " did not " + expected.toLowerCase() + " as expected."
+                    );
+                    equal(
+                        test.error,
+                        undef,
+                        "There should not have been an error in test " + test.name
+                    );
+                }
+                
+                start();
+            }
+        });
+        t.module("tests", function () {
+            var x = 3;
+            this.test("a", function (assert) {
+                assert.that(x).is(3).since("x should be three");
+            });
+            this.test("b", function (assert) {
+                assert(x).is(3).since("x should be three");
+            });
+            this.test("c", function (assert) {
+                assert(x).is(3)("x should be three");
+            });
+            this.test("d", function (assert) {
+                assert.that(x).is(4)("x should be four");
+            });
+            this.test("e", function (assert) {
+                assert.that(x).is.not(4).since("x should not be four");
+            });
+            this.test("f", function (assert) {
+                assert(x).not(3)("x should not be three");
+            })
+        });
+        t.run();
+    });
+    asyncTest("Assertions can be reused", function () {
+        expect(1);
+        var t;
+        t = new Tyrtle({
+            callback : function () {
+                equal(this.passes, 1, "There should be one pass");
+                start();
+            }
+        });
+        t.module("foo", function () {
+            this.test("a", function (assert) {
+                var x = 3, a;
+                a = assert(x).is(3);
+                a("works the first time");
+                a("works the second time");
+            });
+        });
+        t.run();
+    });
+});
