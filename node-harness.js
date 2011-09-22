a8a1cd0a1bfd7a290231c55e629dfcdaef92b2d1
Nick Fisher <nick.fisher@numberfour.eu>
2011-09-22 23:07:34 +1000

    Moved the test harness into the root directory

--- node-harness.js
+++ node-harness.js
@@ -1,72 +1,75 @@
-var Tyrtle = require('../Tyrtle'),
+var Tyrtle = require('./Tyrtle'),
     renderer,
     fs = require('fs'),
     path = require('path'),
     glob,
     tests,
-    args = require('optimist')
-        .boolean("monochrome")
-        .describe("monochrome", "Output without any colors")
-        .boolean("only-errors")
-        .describe("only-errors", "Only show tests which fail")
-        .string("list")
-        .describe("list", "If this is set, then the files specified should contain a JSON object specifying the paths "
-            + "of other files to include"
-        )
-        .boolean('dry')
-        .alias('dry', 'dry-run')
-        .describe('dry', "Perform a dry run. In this mode, none of the test modules are loaded. Instead, a list of the "
-                  + "files which *would* have been loaded is displayed and the program exits. This is useful for "
-                  + "testing file-matching patterns")
-        .string('renderer')
-        .alias('renderer', 'r')['default']('renderer', 'node')
-        .usage("[1] : $0 [options] [--] file [file [file ...]]\n"
-             + "[2] : $0 [options] --list fileList\n"
-             + "\n"
-             + "In method [1], wildcards can be used, eg: $0 *.test.js test.*.js\n"
-             + "To use recursive matching (through subdirectories) you will need to quote the strings,\n"
-             + "eg: $0 \"tests/**.js\" \"vendors/tests/**\""
-             + "\n"
-             + "In method [2], paths in the file list are relative to the list itself. Wildcard patterns can also be \n"
-             + "used inside a file list, however no extra quoting is required."
-        )
-        .wrap(80)
-        .check(function (args) {
-            // this function throws so that optimist doesn't print out the body of this function
-            if (args._.length === 0) {
-                if (!args.list) {
-                    throw "";
-                }
-            } else if (args.list) {
-                throw "";
-            }
-            if (args.renderer) {
-                try {
-                    if (/^\.{0,2}\//.test(args.renderer)) { // relative or absolute path given
-                        // load the path given
-                        console.log('wtf');
-                        renderer = require(args.renderer);
-                    } else {
-                        try {
-                            // load from the renderers folder
-                            renderer = require('../renderers/' + args.renderer);
-                        } catch (ee) {
-                            // try loading from node_modules or whatever
-                            renderer = require(args.renderer);
-                        }
-                    }
-                } catch (e) {
-                    throw "Renderer [" + args.renderer + "] not found";
-                }
-            }
-        })
-        .argv,
+    args,
     color,
     loadFiles,
     inp, inptext,
     runTests,
     oldCwd
 ;
+
+args = require('optimist')
+    .boolean("monochrome")
+    .describe("monochrome", "Output without any colors")
+    .boolean("only-errors")
+    .describe("only-errors", "Only show tests which fail")
+    .string("list")
+    .describe("list", "If this is set, then the files specified should contain a JSON object specifying the paths "
+        + "of other files to include"
+    )
+    .boolean('dry')
+    .alias('dry', 'dry-run')
+    .describe('dry', "Perform a dry run. In this mode, none of the test modules are loaded. Instead, a list of the "
+              + "files which *would* have been loaded is displayed and the program exits. This is useful for "
+              + "testing file-matching patterns")
+    .string('renderer')
+    .alias('renderer', 'r')['default']('renderer', 'node')
+    .usage("[1] : $0 [options] [--] file [file [file ...]]\n"
+         + "[2] : $0 [options] --list fileList\n"
+         + "\n"
+         + "In method [1], wildcards can be used, eg: $0 *.test.js test.*.js\n"
+         + "To use recursive matching (through subdirectories) you will need to quote the strings,\n"
+         + "eg: $0 \"tests/**.js\" \"vendors/tests/**\""
+         + "\n"
+         + "In method [2], paths in the file list are relative to the list itself. Wildcard patterns can also be \n"
+         + "used inside a file list, however no extra quoting is required."
+    )
+    .wrap(80)
+    .check(function (args) {
+        // this function throws so that optimist doesn't print out the body of this function
+        if (args._.length === 0) {
+            if (!args.list) {
+                throw "";
+            }
+        } else if (args.list) {
+            throw "";
+        }
+        if (args.renderer) {
+            try {
+                if (/^\.{0,2}\//.test(args.renderer)) { // relative or absolute path given
+                    // load the path given
+                    renderer = require(args.renderer);
+                } else {
+                    try {
+                        // load from the renderers folder
+                        renderer = require('./renderers/' + args.renderer);
+                    } catch (ee) {
+                        // try loading from node_modules or whatever
+                        renderer = require(args.renderer);
+                    }
+                }
+            } catch (e) {
+                throw "Renderer [" + args.renderer + "] not found";
+            }
+        }
+    })
+    .argv
+;
+
 //console.log(require('optimist'));
 //console.log(args);
 //process.exit(0);
